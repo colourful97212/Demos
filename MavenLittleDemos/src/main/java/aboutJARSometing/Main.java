668707e448cbee1d7a9f7753e511c11e25eb667e
gonglige <gonglige@dsgdata.com>
2020-08-26 15:57:14 +0800

    jar的一些事

---
+++ MavenLittleDemos/src/main/java/aboutJARSometing/Main.java
@@ -0,0 +1,18 @@
+package aboutJARSometing;
+
+import aboutSortSomething.JSONArraySortDemo;
+
+/**
+ * @Description
+ * @Version
+ * @Date 8/26/2020
+ * @Author Gon
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JSONArraySortDemo jsonArraySortDemo = new JSONArraySortDemo();
+        jsonArraySortDemo.main(new String[]{"gg"});
+    }
+}
