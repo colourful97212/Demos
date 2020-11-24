27d060d9a81c744b790a54856a694d11aaa42a4b
gonglige <gonglige@dsgdata.com>
2020-11-24 18:35:37 +0800

    something

--- MavenLittleDemos/src/main/java/aboutThreadSomething/aboutThreadPool/Demo1.java
+++ MavenLittleDemos/src/main/java/aboutThreadSomething/aboutThreadPool/Demo1.java
@@ -16,9 +16,21 @@ public class Demo1
     public static void main(String[] args)
     {
         //ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        // 可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
+
+
         //ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+        // 可控制线程最大并发数，超出的线程会在队列中等待
+
+
         //ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        // 它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO, 优先级)执行。
+
+
         ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+        //定长线程池，支持定时及周期性任务执行。
+
+
         TestThreadVo2 testThreadVo2 = new TestThreadVo2();
         TestThreadVo testThreadVo = new TestThreadVo(testThreadVo2);
 
--- MavenLittleDemos/src/main/java/aboutThreadSomething/aboutThreadPool/TestThreadVo.java
+++ MavenLittleDemos/src/main/java/aboutThreadSomething/aboutThreadPool/TestThreadVo.java
@@ -47,4 +47,10 @@ public class TestThreadVo implements Runnable
         testThreadVo2.stop();
         controller = false;
     }
+
+    public static void main(String[] args)
+    {
+        String str = "<br>大数据组件基础开始检测<br>大数据组件基础配置检测完成<br>跳过大数据组件上传步骤";
+        System.out.println(str.substring(0, str.lastIndexOf("<br>")));
+    }
 }
