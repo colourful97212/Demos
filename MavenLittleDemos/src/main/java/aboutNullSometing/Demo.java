98e521b4f9e6920265b26ad9fce15687cf475c9c
gonglige <colourful97212@gmail.com>
2021-04-22 11:19:31 +0800

    关于空的一些事

---
+++ MavenLittleDemos/src/main/java/aboutNullSometing/Demo.java
@@ -0,0 +1,18 @@
+package aboutNullSometing;
+
+/**
+ * @Description
+ * @Version
+ * @Date 2021/4/21
+ * @Author Gon
+ */
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        String str = null;
+        System.out.println(String.valueOf(str));
+
+        System.out.println(str.toString());
+    }
+}
